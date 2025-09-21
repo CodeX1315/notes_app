efdc6de5058ea211a040ac033d3ae3eef2fcbfb4
CodeX1315 <deepakmahade777@gmail.com>
2025-09-21 20:20:41 +0530

    feat: archive page

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+
+export const Sidebar = () => {
+  const getStyle = ({ isActive }) => {
+    return isActive
+      ? "bg-pink-200 flex items-center gap-2 m-2 rounded-r-3xl p-3 "
+      : "hover:bg-pink-200 hover:rounded-r-3xl p-2.5 flex items-center gap-2 m-2 hover:text-pink-800";
+  };
+  return (
+    <aside className="w-[170px] h-full bg-pink-100 p-4 flex flex-col gap-3 shadow-2xl text-lg font-medium">
+      <NavLink to="/" className={getStyle}>
+        <span class="material-icons-outlined">home</span>
+        <span>Home</span>
+      </NavLink>
+      <NavLink to="/archive" className={getStyle}>
+        <span class="material-icons-outlined">archive</span>
+        <span>Archive</span>
+      </NavLink>
+      <NavLink to="/important" className={getStyle}>
+        <span class="material-icons-outlined">label_important</span>
+        <span>Important</span>
+      </NavLink>
+      <NavLink to="bin" className={getStyle}>
+        <span class="material-icons-outlined">delete</span>
+        <span>Bin</span>
+      </NavLink>
+    </aside>
+  );
+};
---
+++ src/pages/Archive/index.jsx
@@ -0,0 +1,30 @@
+import { Fragment } from "react";
+import Navbar from "../../components/Navbar";
+import { Sidebar } from "../../components/Sidebar";
+import { useNotes } from "../../context/notes-context";
+import { NotesCard } from "../../components/NotesCard";
+
+export const Archive = () => {
+
+    const { archive } = useNotes();
+  return (
+    <Fragment>
+      <Navbar />
+      <main className="flex h-[calc(100vh-64px)] relative ">
+        <Sidebar />
+        <div className="flex flex-wrap absolute ml-44">
+          {archive?.length > 0 &&
+            archive.map(({ id, title, text, isPinned }) => (
+              <NotesCard
+                key={id}
+                id={id}
+                title={title}
+                text={text}
+                isPinned={isPinned}
+              />
+            ))}
+        </div>
+      </main>
+    </Fragment>
+  );
+};
